c7d8a442c16a63c5151dcf1d1510b9cdd41b1176
rlnrlnworld <diiidhe@gmail.com>
2024-09-03 10:11:12 +0900

    Add loading animation

--- dist/index.4d6bcbeb.js
+++ dist/index.4d6bcbeb.js
@@ -805,10 +805,12 @@ const store = new (0, _setup.Store)({
     searchText: "",
     page: 1,
     pageMax: 1,
-    movies: []
+    movies: [],
+    loading: false
 });
 exports.default = store;
 const searchMovies = async (page)=>{
+    store.state.loading = true;
     store.state.page = page;
     if (page === 1) store.state.movies = [];
     const res = await fetch(`https://www.omdbapi.com/?apikey=7035c60c&s=${store.state.searchText}&page=${page}`);
@@ -818,6 +820,7 @@ const searchMovies = async (page)=>{
         ...Search
     ];
     store.state.pageMax = Math.ceil(Number(totalResults) / 10) || 0;
+    store.state.loading = false;
 };
 
 },{"../core/setup":"7BeA3","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"8UDl3":[function(require,module,exports) {
@@ -834,16 +837,22 @@ class MovieList extends (0, _setup.Component) {
         (0, _movieDefault.default).subscribe("movies", ()=>{
             this.render();
         });
+        (0, _movieDefault.default).subscribe("loading", ()=>{
+            this.render();
+        });
     }
     render() {
         this.el.classList.add("movie-list");
         this.el.innerHTML = `
             <div class="movies"></div>
+            <div class="the-loader hide"></div>
         `;
         const moviesEl = this.el.querySelector(".movies");
         moviesEl.append(...(0, _movieDefault.default).state.movies.map((movie)=>new (0, _movieItemDefault.default)({
                 movie
             }).el));
+        const loaderEl = this.el.querySelector(".the-loader");
+        (0, _movieDefault.default).state.loading ? loaderEl.classList.remove("hide") : loaderEl.classList.add("hide");
     }
 }
 exports.default = MovieList;
